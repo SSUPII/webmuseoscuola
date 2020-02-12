2b4c181db829a24cdb60251f50b41df2d5998d71
Sergio <mari.sergio2001@gmail.com>
2020-02-12 11:33:41 +0100

    Continue working on main page

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <link rel="icon" href="./favicon.ico" />
@@ -13,7 +13,21 @@
         <div class=bodyCountainer>
             <p class=title>Choose a city. You will see every museum and artist from that city!</p>
 			<div class=content>
-				
+			  <?php
+			    $connection = mysqli_connect("127.0.0.1","user","","museo");
+			    if($connection != FALSE){
+			      $sql = "SELECT nome FROM citta ORDER BY nome";
+			      $result = mysqli_query($connection,$sql);
+			      if($result != FALSE){
+			        while($data = mysqli_fetch_assoc($result)){
+			          
+			        }
+			      }
+			    }
+			    else {
+	          echo "<p>An error has occurred</p>".mysqli_connect_error();
+			    }
+			  ?>
 			</div>
         </div>
     </body>
